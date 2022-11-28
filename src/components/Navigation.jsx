12c8fe40202dea6c838fde3953071fee0898dc0b
Arpit Patel <arpit.patel194@gmail.com>
2022-11-28 11:37:17 +0530

    Add responsive Menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 import classes from "./Navigation.module.css";
+import { BiMenu } from "react-icons/bi";
 
 const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={classes.container}>
       <div className={classes.brand}>Estatery</div>
-      <div className={classes.navLinks}>
-        <div className={classes.navLink}>Rent</div>
-        <div className={classes.navLink}>Buy</div>
-        <div className={classes.navLink}>Sell</div>
-        <select className={classes.navLink}>
-          <option>Manage Property</option>
-        </select>
-        <select className={classes.navLink}>
-          <option>Resources</option>
-        </select>
+      <div
+        className={`${classes.menuContainer} ${menuOpen && classes.menuOpen}`}
+      >
+        <div className={classes.navLinks}>
+          <div className={classes.navLink}>Rent</div>
+          <div className={classes.navLink}>Buy</div>
+          <div className={classes.navLink}>Sell</div>
+          <select className={classes.navLink}>
+            <option>Manage Property</option>
+          </select>
+          <select className={classes.navLink}>
+            <option>Resources</option>
+          </select>
+        </div>
+        <div className={classes.navButtons}>
+          <Button type="light" padding="5px 20px">
+            Login
+          </Button>
+          <Button type="dark" padding="5px 20px">
+            Sign up
+          </Button>
+        </div>
       </div>
-      <div className={classes.navButtons}>
-        <Button type="light" padding="5px 20px">
-          Login
-        </Button>
-        <Button type="dark" padding="5px 20px">
-          Sign up
-        </Button>
-      </div>
+      <BiMenu className={classes.menu} onClick={() => setMenuOpen(!menuOpen)} />
     </nav>
   );
 };
